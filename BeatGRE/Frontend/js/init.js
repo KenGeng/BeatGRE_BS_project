3f6575263c39590fadc614e1effa6bcb7a46bdb7
KenGeng <gbvk123@163.com>
2018-06-28 22:38:37 +0800

    添加用户主页、设置、背诵的前端网页;添加excel转json的script

--- BeatGRE/Frontend/js/init.js
+++ BeatGRE/Frontend/js/init.js
@@ -1,7 +1,13 @@
 (function($){
-  $(function(){
+  // $(function(){
+  //
+  //   $('.button-collapse').sideNav();
+  //
+  // }); // end of document ready
 
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+        // $('.collapsible').collapsible('open');
+    });
 })(jQuery); // end of jQuery name space
+
