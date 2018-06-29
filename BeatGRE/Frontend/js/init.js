d322c826dd1db97b7aaa39e615671167fb78e0d2
KenGeng <gbvk123@163.com>
2018-06-29 14:42:11 +0800

    单词卡的折叠done

--- BeatGRE/Frontend/js/init.js
+++ BeatGRE/Frontend/js/init.js
@@ -5,9 +5,24 @@
   //
   // }); // end of document ready
 
-    $(document).ready(function(){
-        $('.collapsible').collapsible();
-        // $('.collapsible').collapsible('open');
+    // $(document).ready(function(){
+    //     // $('.collapsible').collapsible();
+    //     $('.collapsible').collapsible({
+    //         accordion : true
+    //     });
+    // });
+
+    $('.collapsible').collapsible({
+        accordion : true
+    });
+    let elem = document.querySelectorAll('.collapsible.expandable');
+
+
+    let instanceCollapsible = M.Collapsible.init(elem, {
+        accordion: true,
+        onOpenStart: function(){
+            console.log('collasible Open Star12t');
+        }
     });
 })(jQuery); // end of jQuery name space
 
--- BeatGRE/Frontend/js/recite.js
+++ BeatGRE/Frontend/js/recite.js
@@ -1,13 +1,18 @@
 (function($){
-    $(function(){
+    $(document).ready(function(){
 
+        $('.collapsible').collapsible({
+            accordion : true
+        });
         showWord();
-    }); // end of document ready
+    });
+
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     // showWord();
+    // });
+
 })(jQuery); // end of jQuery name space
 function showWord() {
-
-// var dialog = document.querySelector('#dialog_tpl');
-
 
     $.ajax({url:'http://localhost:5222/recite',
         processData: false,
@@ -24,13 +29,32 @@ function showWord() {
             console.log(ob.result);
             console.log(ob.worddata.wordpatch);
             var batch=ob.worddata.wordpatch;
-            // var temp=JSON.parse(res.data);
-            // console.log("haha"+temp.wordpatch[0].word);
-            for (var i = 0; i < 9; i++) {
-                var dialog = document.querySelector('#dialog_tpl');
-                dialog.content.getElementById("asd").innerHTML=batch[i].word;
-                document.getElementById("kkk").appendChild(dialog.content.cloneNode(true));
+
+            // var dialog = document.querySelector('#kkk');
+            for (var i = 0; i < 10; i++) {
+
+                // var inhtml ='<ul class="collapsible expandable">';
+                // inhtml+='<li class>';
+                // inhtml+='<div class="collapsible-header">';
+                // inhtml+='<i class="material-icons">filter_drama</i>';
+                // inhtml+='<span id="word_content">123</span>';
+                // inhtml+='<span class="badge light-green-text">check</span>';
+                // inhtml+='</div>';
+                // // style="display: block;"
+                // inhtml+='<div class="collapsible-body " ><p>Lorem ipsum dolor sit amet.</p></div>';
+                // inhtml+='</li>';
+                // inhtml+=' </ul>';
+                //
+                // dialog.innerHTML+=inhtml;
+
+                // dialog.content.getElementById("word_content").innerHTML=batch[i].word;
+               // dialog.content.getElementById("translation").innerHTML=batch[i].translation;
+                var singleline = document.querySelector("#word_expandable");
+                singleline.content.getElementById("word_content").innerText=batch[i].word;
+                singleline.content.getElementById("translation_content").innerText=batch[i].translation;
+                document.getElementById("kkk").appendChild(singleline.content.cloneNode(true));
             }
+            $('.collapsible').collapsible();//一行代码 花了7个小时 哎
 
         },
         error:function (res) {
